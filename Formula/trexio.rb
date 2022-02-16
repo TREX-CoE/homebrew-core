d3d5bf5bab6843b01388cd19bfafd2a05fd60ad4
q-posev <q-posev@trex-coe>
2022-02-16 11:02:32 +0100

    trexio 2.1.0 (post-audit fixes)

--- Formula/trexio.rb
+++ Formula/trexio.rb
@@ -4,18 +4,20 @@ class Trexio < Formula
   url "https://github.com/TREX-CoE/trexio/releases/download/v2.1.0/trexio-2.1.0.tar.gz"
   sha256 "232866c943b98fa8a42d34b55e940f7501634eb5bd426555ba970f5c09775e83"
   license "BSD-3-Clause"
-  head "https://github.com/TREX-CoE/trexio", branch: "master"
+  head "https://github.com/TREX-CoE/trexio.git", branch: "master"
+  # This cask will not be merged in the upstream homebrew due to the following reason (from `brew audit`)
+  #   * GitHub repository not notable enough (<30 forks, <30 watchers and <75 stars)
+  # (see https://github.com/Homebrew/brew/blob/master/docs/Acceptable-Formulae.md)
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "gcc" # for gfortran
   depends_on "hdf5"
 
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 
   test do
@@ -31,13 +33,13 @@ class Trexio < Formula
 
         trexio_t* test_file = trexio_open("brew_test.dir", "w", TREXIO_TEXT, &rc);
         assert(test_file != NULL);
-        assert(rc == TREXIO_SUCCESS);        
- 
+        assert(rc == TREXIO_SUCCESS);
+
         rc = trexio_write_nucleus_num(test_file, 666);
-        assert(rc == TREXIO_SUCCESS);        
-       
+        assert(rc == TREXIO_SUCCESS);
+
         rc = trexio_close(test_file);
-        assert(rc == TREXIO_SUCCESS);        
+        assert(rc == TREXIO_SUCCESS);
 
         return 0;
       }
